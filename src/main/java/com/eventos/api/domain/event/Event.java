75d6b9b84c3a02ac244a57f31e97dfa41b44bfdf
FernandoRhenan <fernandorhenan9@gmail.com>
2024-12-20 15:00:12 -0300

    feat: adds domain

---
+++ src/main/java/com/eventos/api/domain/event/Event.java
@@ -0,0 +1,33 @@
+package com.eventos.api.domain.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Table(name = "event")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String title;
+    private String description;
+    private String eventUrl;
+    private String imageUrl;
+    private Boolean remote;
+    private Date date;
+
+}
